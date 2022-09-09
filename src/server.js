eab0d7b517ef6951af3e88a65740e03de2538546
Rishav Sharan <rishav.sharan@live.com>
2022-09-09 23:49:46 +0530

    serving static files

--- src/server.js
+++ src/server.js
@@ -1,6 +1,30 @@
-import { Hono } from 'hono'
+import { Hono, serveStatic } from 'hono'
 const app = new Hono()
 
-app.get('/', (c) => c.text('Hello World! This is Hono!'))
+// app.use('/pub/*', serveStatic({ root: './' }))
+
+app.get('/pub/*', async (c) => {
+    return await c.env.ASSETS.fetch(c.req)
+})
+  
+
+app.get('/', (c) => c.html(
+    /*html*/`
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="UTF-8">
+            <link rel="icon" type="image/x-icon" href="/pub/favicon.ico">
+            <title>A Meaningful Page Title</title>
+        </head>
+        <body>
+
+        The content of the document......
+        <img src="/pub/bm.png" />
+
+        </body>
+    </html>
+    `
+))
 
 export default app
